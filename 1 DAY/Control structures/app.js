17a1e9dc819cb658832f55c8c22b24d5ef06cc1b
iamrajabli <iamrajabli@outlook.com>
2022-03-19 22:50:30 +0400

    Example IF-ELSE

---
+++ 1 DAY/Control structures/app.js
@@ -0,0 +1,11 @@
+'use strict';
+// IF - ELSE IF - ELSE
+
+// V8 Engine kodları yuxarıdan aşağı oxuyur.
+const myAge = 22;
+
+if (myAge > 30) {
+    console.log('true');
+} else {
+    console.log('false');
+}
