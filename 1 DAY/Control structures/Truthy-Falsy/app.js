ff135a57ec446fef79d5143555c3666032fff8f8
iamrajabli <iamrajabli@outlook.com>
2022-03-19 23:51:25 +0400

    Truth-Falsy example 1

---
+++ 1 DAY/Control structures/Truthy-Falsy/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Example 1 - If'in içində yazdığımız bütün şərtlərdən 2 cavab çıxır: True və False
+
+let brithOfYear = 1999;
+
+if (brithOfYear) {
+    console.log('TRUE');
+} else {
+    console.log('FALSE');
+}
