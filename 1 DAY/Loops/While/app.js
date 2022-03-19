15148ef740343d8b1b540ec51e644be70c00b677
iamrajabli <iamrajabli@outlook.com>
2022-03-20 00:58:06 +0400

    Example 1

---
+++ 1 DAY/Loops/While/app.js
@@ -0,0 +1,37 @@
+'use strict';
+// Döngülər təkrar ediləcək işləri bir sətirdə yazmaqda kömək edirlər
+
+// Example 1 
+// While algoritim width Switch
+let i = 0;
+
+switch (true) {
+    case i < 5:
+        i = 1;
+        console.log(i);
+
+    case i < 5:
+        i = 2;
+        console.log(i);
+
+    case i < 5:
+        i = 3;
+        console.log(i);
+
+    case i < 5:
+        i = 4;
+        console.log(i);
+
+    case i < 5:
+        i = 5;
+        console.log(i);
+
+}
+
+// While 
+let v = 1;
+
+while (v <= 5) {
+    console.log(v);
+    v++;
+}
