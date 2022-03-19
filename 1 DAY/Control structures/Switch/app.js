67c0e1932678cce8f175ed8874a4058842ff4e6b
iamrajabli <iamrajabli@outlook.com>
2022-03-19 23:32:58 +0400

    File organize

---
+++ 1 DAY/Control structures/Switch/app.js
@@ -0,0 +1,39 @@
+'use strict';
+
+let dayOfWeek = 1;
+
+
+// Switch Example 
+
+switch (dayOfWeek) {
+    case 1:
+        console.log('Bazar ertesi');
+        break;
+
+    case 2:
+        console.log('Cersembe axsami');
+        break;
+
+    case 3:
+        console.log('Cersembe');
+        break;
+
+    case 4:
+        console.log('Cume axsami');
+        break;
+
+    case 5:
+        console.log('Cume');
+        break;
+
+    case 6:
+        console.log('Shenbe');
+        break;
+
+    case 7:
+        console.log('Bazar');
+        break;
+
+    default:
+        console.log('Bele gun yoxdur!');
+}
