078f306d4162b395b915e89f5c3fc3c2e1e2b792
iamrajabli <iamrajabli@outlook.com>
2022-03-20 00:10:44 +0400

    Example 1

---
+++ 1 DAY/Loops/For/app.js
@@ -0,0 +1,34 @@
+'use strict';
+// Döngülər təkrar ediləcək işləri bir sətirdə yazmaqda kömək edirlər
+
+// Example 1 
+// For algoritim width Switch
+let i = 0;
+
+switch (true) {
+    case i < 5:
+        i = 1;
+        console.log(i);
+
+    case i < 5:
+        i = 2;
+        console.log(i);
+
+    case i < 5:
+        i = 3;
+        console.log(i);
+
+    case i < 5:
+        i = 4;
+        console.log(i);
+
+    case i < 5:
+        i = 5;
+        console.log(i);
+
+}
+
+// For 
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
