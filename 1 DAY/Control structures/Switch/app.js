be39c4c4912e4ad0224617be71cdbd2258638a31
iamrajabli <iamrajabli@outlook.com>
2022-03-19 23:40:09 +0400

    Example 2 - Why break?

--- 1 DAY/Control structures/Switch/app.js
+++ 1 DAY/Control structures/Switch/app.js
@@ -1,9 +1,9 @@
 'use strict';
 
-let dayOfWeek = 2;
+let dayOfWeek = 4;
 
 
-// Switch Example 
+// Switch Example 1
 
 switch (dayOfWeek) {
     case 1:
@@ -17,6 +17,39 @@ switch (dayOfWeek) {
     case 3:
         console.log('Cersembe');
         break;
+
+    case 4:
+        console.log('Cume axsami');
+        break;
+
+    case 5:
+        console.log('Cume');
+        break;
+
+    case 6:
+        console.log('Shenbe');
+        break;
+
+    case 7:
+        console.log('Bazar');
+        break;
+
+    default:
+        console.log('Bele gun yoxdur!');
+}
+
+// Switch Example 2 
+// Switch Break nəyə lazımdır? - V8 engine programı yuxarıdan aşağıya oxuyarkən dayOfWeek'in uyğun gələn sətrindən başlayarak gördüyü birinci breakın şərti də daxil olmaqla ekrana yazdırır. Əgər dayOfWeek'in sətirində  break varsa sadəcə o sətiri oxuyur. Əgər dayOfWeek'in sətirindən sonra break olmayan sətir varsa onları yenə də oxumur.
+
+switch (dayOfWeek) {
+    case 1:
+        console.log('Bazar ertesi');
+
+    case 2:
+        console.log('Cersembe axsami');
+
+    case 3:
+        console.log('Cersembe');
 
     case 4:
         console.log('Cume axsami');
