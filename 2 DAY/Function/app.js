72b529d507d452017da275195738a974bf5622ee
iamrajabli <iamrajabli@outlook.com>
2022-03-20 13:16:40 +0400

    Example 2

--- 2 DAY/Function/app.js
+++ 2 DAY/Function/app.js
@@ -9,3 +9,17 @@ for (let i = 0; i <= 100; i++) {
     calFor += i;
 }
 console.log(calFor);
+
+// Example 2
+// İndi 0 dan deyildə 5 dən 100 ə qədər deyildə 500 qədər lazımdır. Daha rahat olmağı üçün funksiya ilə yazaq
+
+function calculator(start, end) {
+    let calc = 0;
+    for (let i = start; i <= end; i++) {
+        calc += i;
+    }
+    console.log(calc);
+}
+
+calculator(5, 111);
+calculator(52, 142);
