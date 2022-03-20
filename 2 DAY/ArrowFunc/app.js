2e97e025c7005bd55c60c68d7a4a6ca8890a8775
iamrajabli <iamrajabli@outlook.com>
2022-03-20 15:06:42 +0400

    Example 1

---
+++ 2 DAY/ArrowFunc/app.js
@@ -0,0 +1,12 @@
+'use strict';
+// ES6 ilə gələn FAT ARROW 
+
+// Example 1 
+// Dəyişkənə funksiya tanımlamaq olar. Bu halda dəyişkəni funksiya kimi çağırmaq lazımdır.
+
+
+const myFunc = function myName(text) {
+    console.log(text);
+}
+
+myFunc("Hikmat");
