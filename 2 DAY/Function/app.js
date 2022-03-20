2b96352675137b623578f90df630b547b6b1de39
iamrajabli <iamrajabli@outlook.com>
2022-03-20 13:16:18 +0400

    Example 1

---
+++ 2 DAY/Function/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Example 1
+// Funksiyasız FOR - 1 dən 100 ə qədər ədədlərin cəmi lazımdır
+
+let calFor = 0;
+
+for (let i = 0; i <= 100; i++) {
+    calFor += i;
+}
+console.log(calFor);
