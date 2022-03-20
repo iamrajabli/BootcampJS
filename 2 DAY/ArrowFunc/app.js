e4733a0e65033ff203ba00f94ea4fd45e8130f37
iamrajabli <iamrajabli@outlook.com>
2022-03-20 15:10:15 +0400

    Example 2

--- 2 DAY/ArrowFunc/app.js
+++ 2 DAY/ArrowFunc/app.js
@@ -4,9 +4,18 @@
 // Example 1 
 // Dəyişkənə funksiya tanımlamaq olar. Bu halda dəyişkəni funksiya kimi çağırmaq lazımdır.
 
-
 const myFunc = function myName(text) {
     console.log(text);
 }
 
 myFunc("Hikmat");
+
+
+// Example 2
+// Fat arrow ilə funksiya
+
+const fatArrow = () => {
+    console.log('Fat Arrow');
+}
+
+fatArrow();
