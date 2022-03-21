3731a140bca3943201fcf756daf9218a8e79c184
iamrajabli <iamrajabli@outlook.com>
2022-03-21 20:58:09 +0400

    Example 1

---
+++ 3 DAY/Array methods/app.js
@@ -0,0 +1,8 @@
+'use strict';
+
+let personNames = ['Hikmat', 'Nail', 'Yusif'];
+
+// Bəzi önəmli metodlar
+// Example 1
+// toString - mətn kimi ekrana çıxarır. Hər veriləndən sonra vergül qoyulur. 
+console.log(personNames.toString());
