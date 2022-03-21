f2ecd9a45b8d0c25ec30ec61dabdea08ca8cb9aa
iamrajabli <iamrajabli@outlook.com>
2022-03-21 21:37:51 +0400

    Example 1

--- 3 DAY/Object/app.js
+++ 3 DAY/Object/app.js
@@ -13,16 +13,3 @@ let Hikmat = {
     }
 }
 console.log(Hikmat.ageCal());
-
-// Example 2
-let Nail = {
-    name: 'Nail',
-    surname: 'Rajabli',
-    birthOfYear: 1999,
-    height: 185,
-    weight: 85,
-    ageCal: function(year) { // Objectin içində funksiya yazmaq olar.
-        return 2022 - year; // Example 3'də daha fərqli istifadə görmək olar.
-    }
-}
-console.log(Nail.ageCal(Nail.birthOfYear));
