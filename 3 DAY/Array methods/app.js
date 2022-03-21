00374dd7855aa69c93e5005155d9325ca5277025
iamrajabli <iamrajabli@outlook.com>
2022-03-21 20:58:52 +0400

    Example 3

--- 3 DAY/Array methods/app.js
+++ 3 DAY/Array methods/app.js
@@ -10,3 +10,8 @@ console.log(personNames.toString());
 // Example 2
 // join - vergüllə deyildə istədiyimiz simvolla ekrana çıxara bilirik.
 console.log(personNames.join(" _   "));
+
+// Example 3
+// Index nömrəsini yazar deyil, ən son indeksdən sonraki indexə verilən əlavə etmə
+console.log(personNames.push("Rajab")); // Push massivin sonuncu elementinin indeksi + 1 dir.
+console.log(personNames.join(" , "));
