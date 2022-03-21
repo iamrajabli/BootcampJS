d46ff59742527ba944e001d5aeaed862b9595d13
iamrajabli <iamrajabli@outlook.com>
2022-03-21 19:53:22 +0400

    Example 6

--- 3 DAY/Arrays/app.js
+++ 3 DAY/Arrays/app.js
@@ -22,15 +22,79 @@ console.log(cars);
 
 // Example 4 
 // Massivdə olmayan sıraya əlavə etmə
-// Bu halda 20 ci sıraya əlavə ediləcək. Amma massivin uzunluğu 21 olacaq. Yəni 3 cü sıradan 20 ə qədər boş verilənlər olacaq.
+// Bu halda 6 ci sıraya əlavə ediləcək. Amma massivin uzunluğu 7 olacaq. Yəni 3 cü sıradan 6 ə qədər boş verilənlər olacaq.
 
-cars[20] = 'Porsche';
+cars[6] = 'Porsche';
 console.log(cars);
-console.log(cars[10]); // boş olan veriləni çağırmağa çalışdıqda UNDEFINED alarıq.
+console.log(cars[5]); // boş olan veriləni çağırmağa çalışdıqda UNDEFINED alarıq.
 
 // Example 5
 // For array istifadəsi
 
 for (let i = 0; i < cars.length; i++) {
     console.log(cars[i]);
+}
+
+console.log('****************');
+// Example 6 
+// For of array istifadəsi
+
+for (let index of cars) {
+    console.log(index);
+}
+console.log('****************');
+
+// İşləmə prinsipi: let ilə yaratdığımız dəyişkən cars massivinin uzunluğunu təyin edir və hər dəfəsində index sırasına görə verilənləri index dəyişkəninə təyin edir. Aşağıdaki kimi:
+
+let indexArr;
+let i = 0;
+
+switch (true) {
+    case i < cars.length:
+        indexArr = cars[i];
+        i++;
+        console.log(indexArr);
+
+    case i < cars.length:
+        indexArr = cars[i];
+        i++;
+        console.log(indexArr);
+
+    case i < cars.length:
+        indexArr = cars[i];
+        i++;
+        console.log(indexArr);
+
+    case i < cars.length:
+        indexArr = cars[i];
+        i++;
+        console.log(indexArr);
+
+    case i < cars.length:
+        indexArr = cars[i];
+        i++;
+        console.log(indexArr);
+
+    case i < cars.length:
+        indexArr = cars[i];
+        i++;
+        console.log(indexArr);
+
+    case i < cars.length:
+        indexArr = cars[i];
+        i++;
+        console.log(indexArr);
+
+}
+console.log('****************');
+
+// Və ya 
+
+i = 0;
+while (i < cars.length) {
+
+    indexArr = cars[i];
+    console.log(indexArr);
+    i++;
+
 }
