eea19138e18a9dca887e8e2b4bcdd89b57dca8b0
iamrajabli <iamrajabli@outlook.com>
2022-03-21 19:22:56 +0400

    Example 1

---
+++ 3 DAY/Arrays/app.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// Verilənləri bir arada tutmaq üçün massivlərdən istifadə olunur
+// Example 1 
+// Massivlərin indeksi 0 dan başlayır. Yəni 3 dənə veriləni olan massivin indeksi: 0, 1, 2;
+
+let cars = ['Bmw', 'Mercedes', 'Audi'];
+console.log(cars);
