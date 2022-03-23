64b34a5f5318d4049c329e8d804384c00cef4d77
iamrajabli <iamrajabli@outlook.com>
2022-03-23 21:59:43 +0400

    DOM started

---
+++ 5 DAY/DOM/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Javacript'ə aid olan bütün məlumatlar window içindədir
+let itemWindow = window;
+console.log(itemWindow);
+
+// DOM - Document Object Model - window'un içində yer alır
+// Document sayəsində brauzerdəki HTML teglərinə bağlanmaq olar
+let itemDocument = document;
+console.log(itemDocument);
