a32929f1f4af3b80bfeeb0dd7ff18da9ba79469b
iamrajabli <iamrajabli@outlook.com>
2022-03-23 22:33:30 +0400

    Example 1

---
+++ 5 DAY/DOM Tək element/app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// DOM - tək element seçmək
+// ID ə sahib olanları getElementById ilə çağıra bilərik
+// Example 1
+
+const titleId = document.getElementById('title');
+
+console.log(titleId);
