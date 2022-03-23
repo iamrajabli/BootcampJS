044a9bc883e70460b920539ec48c5be857031af2
iamrajabli <iamrajabli@outlook.com>
2022-03-23 22:39:08 +0400

    Example 2

--- 5 DAY/DOM Tək element/app.js
+++ 5 DAY/DOM Tək element/app.js
@@ -3,7 +3,15 @@
 // DOM - tək element seçmək
 // ID ə sahib olanları getElementById ilə çağıra bilərik
 // Example 1
-
 const titleId = document.getElementById('title');
 
-console.log(titleId);
+// Seçdiyimiz element üzərində bir neçə əməliyyat
+// Example 2
+
+// Mətni dəyişdirmək
+titleId.textContent = 'Title edited'; // 1
+titleId.innerText = 'Title edited 2' // 2
+
+// Rəngləri dəyişdirmək
+titleId.style.backgroundColor = 'red';
+titleId.style.color = 'white';
