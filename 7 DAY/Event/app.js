d8df38945965953a01f2b934de310b539d3d934d
iamrajabli <iamrajabli@outlook.com>
2022-03-26 00:21:48 +0400

    Example 2

--- 7 DAY/Event/app.js
+++ 7 DAY/Event/app.js
@@ -1,8 +1,15 @@
 'use strict';
 
-// Example1 - Click
+// Example 1 - Click
 document.querySelector('h1').addEventListener('click', click);
 
 function click(e) {
+    console.log(e.target);
+}
+
+// Example 2 - Dblclick
+document.querySelector('h1').addEventListener('dblclick', dblclick);
+
+function dblclick(e) {
     console.log(e.target);
 }
