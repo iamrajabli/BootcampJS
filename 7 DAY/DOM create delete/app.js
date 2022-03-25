88557415e496c3cdf1a3766abc16cc1ea7e3a8b4
iamrajabli <iamrajabli@outlook.com>
2022-03-26 00:10:17 +0400

    Example 2

--- 7 DAY/DOM create delete/app.js
+++ 7 DAY/DOM create delete/app.js
@@ -1,8 +1,16 @@
 'use strict';
-// Element yaradılması
+
+// Element yaradılması - сreate
 // Example 1
 const a = document.createElement('a');
 a.classList = 'colorBlack resetA btn';
 a.setAttribute('href', 'https://www.github.com/iamrajabli');
 a.textContent = 'My GitHub Profile';
 document.body.appendChild(a);
+
+// Element dəyişdirilməsi - replace
+// Example 2
+const h1 = document.createElement('h1');
+h1.classList = 'colorBlack resetA btn';
+h1.textContent = 'My GitHub Profile';
+document.body.replaceChild(h1, a);
