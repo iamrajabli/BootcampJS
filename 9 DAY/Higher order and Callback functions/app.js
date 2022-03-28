74da49f2e54599b61276a359608049cc6f13e651
iamrajabli <iamrajabli@outlook.com>
2022-03-29 00:32:14 +0400

    Example 2

--- 9 DAY/Higher order and Callback functions/app.js
+++ 9 DAY/Higher order and Callback functions/app.js
@@ -14,5 +14,45 @@ function ikiyeVur(arr) {
 }
 
 ikiyeVur(arr);
-
+console.log("***************************");
 // Verilən massivdəki ədədləri ikiyə vurmaq üçün proqram yazırıq. Amma bizdən ikiyə bölmək üçün də, hər birinin üstünə 5 gəlmək üçün də, hər birindən 3 çıxmaq üçün də proqram istənilir. Bildiyimiz yollarla getsək əgər, məcburuq ki kod təkrarı edək. Ammma higher order və callback anlayışı ilə bu məsələni az kodla həll edə bilərik.
+// Yəni;
+// Example 2
+
+const arrSecond = [4, 6, 8, 10]; // ***
+
+// Burda function process - higher order function
+// parametr callback - callback function
+function process(arr, callback) {
+    let localArr = [];
+    for (let i = 0; i < arr.length; i++) {
+
+        // localArr[i] = arr[i] * 2;
+
+        localArr[i] = callback(arr[i]); // arr[i] = 4,6,8,10 ***
+
+    }
+
+    console.log(localArr);
+}
+
+function ikiVur(int) {
+    return int * 2;
+}
+
+function ikiBol(int) {
+    return int / 2;
+}
+
+function beshGel(int) {
+    return int + 5;
+}
+
+function ucCix(int) {
+    return int - 2;
+}
+
+process(arrSecond, ikiVur);
+process(arrSecond, ikiBol);
+process(arrSecond, beshGel);
+process(arrSecond, ucCix);
