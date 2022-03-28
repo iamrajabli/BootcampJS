c638b4ed6dd93117b24b3971f3ff873adc213f8d
iamrajabli <iamrajabli@outlook.com>
2022-03-29 01:21:23 +0400

    Example 2

--- 9 DAY/Foreach array helper/app.js
+++ 9 DAY/Foreach array helper/app.js
@@ -5,4 +5,18 @@ let arrNorm = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 arrNorm.forEach(function(item, index) {
     console.log(item, index);
-})
+});
+
+
+console.log("***************");
+
+// Example 2 - Algoritm
+let arrAlgoritm = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function forEachAlgoritm(arrAlgoritm) {
+    for (let i = 0; i < arrAlgoritm.length; i++) {
+        console.log(arrAlgoritm[i], i);
+    }
+}
+forEachAlgoritm(arrAlgoritm);
+console.log("***************");
