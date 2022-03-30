d598b4a8dc73cf2834dcfe5df72c7fe8c48755a8
iamrajabli <iamrajabli@outlook.com>
2022-03-30 23:54:17 +0400

    Example 1

--- 11 DAY/app.js
+++ 11 DAY/app.js
@@ -1,1 +1,25 @@
 'use strict';
+// Filter istifadəsi və öz filter metodumuzun callback ilə yazılışı
+// Example 1
+
+let bitkiler = [
+    { ad: 'uzum', nov: 'meyve' },
+    { ad: 'vezeri', nov: 'terevez' },
+    { ad: 'banan', nov: 'meyve' }
+];
+
+// Callback olmadan yazılışı
+function meyveEx1(arr) {
+    let newMeyveArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].nov === 'meyve') {
+            newMeyveArr.push(arr[i])
+        }
+    }
+
+    console.log(newMeyveArr);
+}
+
+meyveEx1(bitkiler);
+
+console.log("************");
