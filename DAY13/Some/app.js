0dc4821a72df3c4894d5628fee3ed23e0a116aaa
iamrajabli <iamrajabli@outlook.com>
2022-04-02 17:35:55 +0400

    Example 2

--- DAY13/Some/app.js
+++ DAY13/Some/app.js
@@ -1,10 +1,28 @@
 'use strict';
 // Some istifadəsi və yazılış qaydası - ən az biri şərti qarşılamalıdır*
 // Example 1
-let arr = [1, 2, 3, 4, 5, 6];
+let _arr = [1, 2, 3, 4, 5, 6];
 
-const result = arr.some(controlArr => {
-    return controlArr > 10; // false *
+const _Ex1 = _arr.some(__controlArr => {
+    return __controlArr > 10; // false *
 });
 
-console.log(result);
+console.log(_Ex1);
+console.log("*********");
+
+// Example 2 - normal yazılışı
+function __controlFromArrEx1(__arr) {
+    let _control = [];
+    for (let index in __arr) {
+        if (__arr[index] > 7) {
+            _control.push(__arr[index]); // Kontrol edilməsi üçün arraya əlavə edildi.
+        }
+    }
+    if (_control.length > 0) {
+        console.log('true');
+    } else {
+        console.log('false');
+    }
+}
+
+__controlFromArrEx1(_arr)
