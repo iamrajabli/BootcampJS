2c8e774aefa285b0a5564a6596f4d3f8a52856fc
iamrajabli <iamrajabli@outlook.com>
2022-04-02 18:02:34 +0400

    Example 3

--- DAY13/Some/app.js
+++ DAY13/Some/app.js
@@ -11,18 +11,39 @@ console.log(_Ex1);
 console.log("*********");
 
 // Example 2 - normal yazılışı
-function __controlFromArrEx1(__arr) {
-    let _control = [];
+function __controlFromArrEx2(__arr) {
+    let _controlArr = [];
     for (let index in __arr) {
-        if (__arr[index] > 7) {
-            _control.push(__arr[index]); // Kontrol edilməsi üçün arraya əlavə edildi.
+        if (__arr[index] > 3) {
+            _controlArr.push(__arr[index]); // Kontrol edilməsi üçün arraya əlavə edildi.
         }
     }
-    if (_control.length > 0) {
+    if (_controlArr.length > 0) {
         console.log('true');
     } else {
         console.log('false');
     }
 }
 
-__controlFromArrEx1(_arr)
+__controlFromArrEx2(_arr);
+console.log("*********");
+
+// Example 3 - callback 
+function __controlFromArrEx3(__arr, __callback) {
+    let _controlVar; // Dəyişkənin massiv olmamasının səbəbi: callback funksiya ilə kontrol zamanı ilk tapdığı dəyəri döndürdüyü üçün massiv yazaraq onun için kontrol etməyə ehtiyac olmadı.
+    for (let index in _arr) {
+        _controlVar = __callback(_arr[index]); // return ilə bura true ya da false göndərdik
+    }
+    if (_controlVar) { // gələn true ya da false ə görə kontrol edilir
+        console.log(_controlVar);
+    } else {
+        console.log(_controlVar);
+    }
+}
+
+function __callBackForEx3(__arr) {
+    return __arr > 2;
+
+}
+
+__controlFromArrEx3(_arr, __callBackForEx3)
