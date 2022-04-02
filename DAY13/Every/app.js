26d25c98aa3e9920d179136b409d56af675b66b2
iamrajabli <iamrajabli@outlook.com>
2022-04-02 18:14:44 +0400

    Example 2

--- DAY13/Every/app.js
+++ DAY13/Every/app.js
@@ -9,3 +9,23 @@ const result = _arr.every(__controlArr => {
 
 console.log(result);
 console.log("*********");
+
+// Example 2 - normal yazılışı
+// Algoritm: Massivin içindəki dəyərlər kontrol edildi və true olanlar yeni massivə əlavə edildi. Sonra köhnə massivlə yeni massivin uzunluqları müqayisə edilidi. Əgər bərabərdirsə demək bütün dəyərlər şərti yerinə gətirir.
+function __controlFromArrEx2(__arr) {
+    let _controlArr = [];
+    for (let index in _arr) {
+        if (_arr[index] > 0) {
+            _controlArr.push(_arr[index]);
+        }
+    }
+
+    if (__arr.length === _controlArr.length) {
+        console.log('true');
+    } else {
+        console.log('false');
+    }
+}
+
+__controlFromArrEx2(_arr);
+console.log("*********");
