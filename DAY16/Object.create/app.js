9121c00175e1b76d24ad798751aecedc0eeb6f89
iamrajabli <iamrajabli@outlook.com>
2022-04-06 15:09:59 +0400

    Example 2

--- DAY16/Object.create/app.js
+++ DAY16/Object.create/app.js
@@ -1,19 +1,45 @@
 'use strict';
 // call və object.create istifadəsi və məntiqi.
-// Example 1
+// Example 1 - normal istifadə
 
 function PersonForEx1(ad, soyad) {
     this.ad = ad; // *
-    this.soyad = soyad;
+    this.soyad = soyad; // *
 }
 
 // DRY - Don't repeat yourself.
 function StudentsForEx1(ad, soyad, yas, univer) {
     this.ad = ad; // təkrar *
-    this.soya = soyad; // təkrar *
+    this.soyad = soyad; // təkrar *
     this.yas = yas;
     this.univer = univer;
 }
 
 const hikmatForEx1 = new StudentsForEx1('Hikmat', 'Rajabli', 22, 'KHNUVD');
-console.log(hikmatForEx1);
+
+function logsForEx1() {
+    console.log(hikmatForEx1);
+    console.log("**********");
+}
+
+
+// Example 2 - call istifadəsi
+function PersonForEx2(ad, soyad) {
+    this.ad = ad; // *
+    this.soyad = soyad; // *
+}
+
+// DRY - Don't repeat yourself.
+function StudentsForEx2(ad, soyad, yas, univer) {
+    PersonForEx2.call(this, ad, soyad); // call metodu sayəsində Person qurucu funksiyasından ad və soyadı çağırdıq. 
+    this.yas = yas;
+    this.univer = univer;
+}
+
+const hikmatForEx2 = new StudentsForEx2('Hikmat', 'Rajabli', 22, 'KHNUVD');
+
+function logsForEx2() {
+    console.log(hikmatForEx2);
+    console.log("**********");
+}
+logsForEx2()
