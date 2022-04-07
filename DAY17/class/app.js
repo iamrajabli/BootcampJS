d2aca9ec7ebe51f677e9be4f48d0db3bfa83c61d
iamrajabli <iamrajabli@outlook.com>
2022-04-07 17:11:07 +0400

    Example 3

--- DAY17/class/app.js
+++ DAY17/class/app.js
@@ -14,7 +14,6 @@ class PersonForEx1 {
         return 2022 - this.yas
     }
 };
-
 const hikmatForEx1 = new PersonForEx1('Hikmat', 'Rajabli', 22);
 
 function logsForEx1() {
@@ -35,12 +34,36 @@ class StudentsForEx2 extends PersonForEx1 {
         return 2021 - this.yas
     }
 }
+const raminForEx2 = new StudentsForEx2('Ramin', 'Azimli', 22, 11);
 
-const ramin = new StudentsForEx2('Ramin', 'Azimli', 22, 11);
-
-function logsForEx1() {
-    console.log(ramin);
-    console.log(ramin.dogumTarixi());
+function logsForEx2() {
+    console.log(raminForEx2);
+    console.log(raminForEx2.dogumTarixi());
     console.log("************");
 };
-logsForEx1()
+
+// Example 3 - static
+const title = 'Aze';
+class StudentsForEx3 {
+    constructor(ad, sinif, yas) {
+        this.ad = ad;
+        this.sinif = sinif;
+        this.yas = yas;
+    }
+
+    dogumTarixi() { // prototip
+        return 2021 - this.yas
+    }
+
+    static showTitle = title;
+}
+
+const raminForEx3 = new StudentsForEx3('Ramin', 11, 16);
+
+function logsForEx3() {
+    console.log(raminForEx3);
+    console.log(raminForEx3.dogumTarixi());
+    console.log(StudentsForEx3.showTitle);
+    console.log("************");
+};
+logsForEx3()
