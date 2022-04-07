32813691e9c2eb1ad01d940f6c8bced8c001efdc
iamrajabli <iamrajabli@outlook.com>
2022-04-07 14:21:13 +0400

    Example 2

--- DAY17/class/app.js
+++ DAY17/class/app.js
@@ -10,7 +10,7 @@ class PersonForEx1 {
         this.yas = yas;
     }
 
-    dogumTarixi = function() { // metod
+    dogumTarixi = function() { // prototip
         return 2022 - this.yas
     }
 };
@@ -21,5 +21,25 @@ function logsForEx1() {
     console.log(hikmatForEx1);
     console.log(hikmatForEx1.dogumTarixi());
     console.log(typeof hikmatForEx1);
+    console.log("************");
+};
+
+// Example 2 - extends, super (call)
+class StudentsForEx2 extends PersonForEx1 {
+    constructor(ad, soyad, yas, sinif) {
+        super(ad, soyad, yas);
+        this.sinif = sinif;
+    }
+
+    dogumTarixi = function() { // prototip (parentden gelen prototipi ezdim)
+        return 2021 - this.yas
+    }
+}
+
+const ramin = new StudentsForEx2('Ramin', 'Azimli', 22, 11);
+
+function logsForEx1() {
+    console.log(ramin);
+    console.log(ramin.dogumTarixi());
     console.log("************");
 };
