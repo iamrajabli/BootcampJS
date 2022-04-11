f3887fbbca998d21c67bfe349f1824d8be14fd86
iamrajabli <iamrajabli@outlook.com>
2022-04-11 13:22:30 +0400

    class DB added

--- DAY19/contact-project-oop/app.js
+++ DAY19/contact-project-oop/app.js
@@ -15,5 +15,29 @@ class Screen {
         this.surname = document.getElementById('surname');
         this.email = document.getElementById('email');
         this.submit__btn = document.getElementById('submit__btn');
+        this.getAllData = new DB();
     }
 }
+
+class DB {
+    constructor() {
+        this.getDB = this.getDBMeth();
+    };
+    getDBMeth() {
+        let allPerson;
+        if (localStorage.getItem('allPerson') === null) {
+            allPerson = [];
+        } else {
+            allPerson = JSON.parse(localStorage.getItem('allPerson'));
+        }
+        return allPerson;
+    };
+
+    setDBMeth(person) {
+        this.getDB.push(person);
+        localStorage.setItem('allPerson', JSON.stringify(this.getDB));
+    }
+};
+
+let tas = new DB();
+console.log(tas.setDBMeth('salam'));
