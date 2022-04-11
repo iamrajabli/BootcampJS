3d2be28665ddea33d36bfab61f01075ebdc30c04
iamrajabli <iamrajabli@outlook.com>
2022-04-11 17:00:06 +0400

    deleteFromLs method completed

--- DAY19/contact-project-oop/app.js
+++ DAY19/contact-project-oop/app.js
@@ -10,14 +10,14 @@ class Person {
 };
 
 class controlInputs {
-    static controlForEmpty(...personInfo) {
+    static controlForEmpty(personInfo) {
         let result = true;
-        personInfo.forEach(element => {
-            if (element < 1) {
+        for (let i in personInfo) {
+            if (personInfo[i] === '') {
                 result = false;
-                return result
+                return result;
             }
-        });
+        }
 
         return result;
     }
@@ -29,27 +29,88 @@ class Screen {
         this.surname = document.getElementById('surname');
         this.email = document.getElementById('email');
         this.submit__btn = document.getElementById('submit__btn');
-        this.form = document.getElementById('form__contact').addEventListener('submit', this.addOrUpdate);
-        // this.getAllData = new DB();
+        this.form = document.getElementById('form__contact').addEventListener('submit', this.addOrUpdate.bind(this));
+        this.tbody__contact = document.querySelector('.tbody__contact');
+        this.tbody__contact.addEventListener('click', this.deleteOrUpdate.bind(this));
+        this.getAllData = new DB();
+        this.showAllPersonInScreen(this.getAllData.getDB);
+    };
+
+
+    showAllPersonInScreen(getDB) {
+        for (let i in getDB) {
+            this.addPersonToList(getDB[i]);
+        }
+    }
+
+    resetInputs() {
+        this.name.value = ''
+        this.surname.value = ''
+        this.email.value = ''
+    };
+
+    addPersonToList(person) {
+        const createdTr = document.createElement('tr');
+        createdTr.innerHTML = `
+        <td>${person.name}</td>
+        <td>${person.surname}</td>
+        <td>${person.email}</td>
+        <td>
+            <button class="btn btn--delete"><i class="fa-solid fa-trash-can"></i></button>
+            <button class="btn btn--edit"><i class="fa-solid fa-pen-to-square"></i></button>
+        </td>               
+        `;
+        this.tbody__contact.appendChild(createdTr);
     };
 
     addOrUpdate(e) {
         e.preventDefault();
         const person = new Person(this.name.value, this.surname.value, this.email.value);
-        const result = controlInputs.controlForEmpty(person.ad, person.surname, person.email);
+        const result = controlInputs.controlForEmpty(person);
 
         if (result) {
-            console.log(true);
+            this.addPersonToList(person);
+            this.getAllData.setDBMeth(person);
+            this.resetInputs();
         } else {
             console.log(false);
         }
     }
+
+    deleteOrUpdate(e) {
+        const thatTR = e.target.parentElement.parentElement;
+        const thatEmail = thatTR.cells[2].textContent;
+        console.log(thatEmail);
+        if (e.target.classList.contains('btn--delete')) {
+            thatTR.remove();
+            this.getAllData.deleteFromLs(thatEmail)
+
+        }
+    }
+
 }
 
 class DB {
     constructor() {
         this.getDB = this.getDBMeth();
     };
+
+    deleteFromLs(personEmail) {
+        // for (let i in this.getDB) {
+        //     if (this.getDB[i].email === personEmail) {
+        //         this.getDB.splice(i, 1);
+        //         localStorage.setItem('allPerson', JSON.stringify(this.getDB));
+        //     }
+        // }
+
+        this.getDB.forEach((element, index) => {
+            if (element.email === personEmail) {
+                this.getDB = this.getDB.splice(index, 1);
+            }
+        });
+
+        localStorage.setItem('allPerson', JSON.stringify(this.getDB));
+    }
     getDBMeth() {
         let allPerson;
         if (localStorage.getItem('allPerson') === null) {
@@ -63,9 +124,10 @@ class DB {
     setDBMeth(person) {
         this.getDB.push(person);
         localStorage.setItem('allPerson', JSON.stringify(this.getDB));
-    }
-};
+    };
 
+
+};
 
 document.addEventListener('DOMContentLoaded', function() {
     new Screen();
