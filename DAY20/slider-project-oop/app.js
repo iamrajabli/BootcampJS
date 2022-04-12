08be37f879d189a0fa7ee281fcde48e8e73a0e6d
iamrajabli <iamrajabli@outlook.com>
2022-04-12 21:26:27 +0400

    class [Models] completed

--- DAY20/slider-project-oop/app.js
+++ DAY20/slider-project-oop/app.js
@@ -6,3 +6,12 @@ class Slider {
     }
 
 }
+
+class Models {
+    constructor() {
+        this.bmw = new Slider('Bmw 418d', 'img/bmw.jpg');
+        this.mazda = new Slider('Mazda CX-3', 'img/mazda.jpg');
+        this.volvo = new Slider('Volvo S60', 'img/volvo.jpg');
+        this.skoda = new Slider('Skoda Superb', 'img/skoda.jpg');
+    }
+}
