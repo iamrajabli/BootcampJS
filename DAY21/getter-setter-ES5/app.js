b7bb67011c28411fa5907015abda03d194066a48
iamrajabli <iamrajabli@outlook.com>
2022-04-13 14:13:31 +0400

    Example 2

--- DAY21/getter-setter-ES5/app.js
+++ DAY21/getter-setter-ES5/app.js
@@ -14,3 +14,25 @@ console.log(PersonForEx1.getFullName()); // Hikmat Rajabli
 // Eger men colden obyektin icindeki melumati deyishmek isteyiremse:
 PersonForEx1.firstName = 'Nail';
 console.log(PersonForEx1.getFullName()); // Nail Rajabli
+console.log("*************");
+
+// Example 2: melumatlari iceriden deyishmek isteyiremse:
+const PersonForEx2 = {
+    firstName: 'Hikmat',
+    lastName: 'Rajabli',
+
+    getFullName: function() {
+        return `${this.firstName} ${this.lastName}`;
+    },
+
+    setFullName: function(value) {
+        const newArr = value.split(" ");
+        this.firstName = newArr[0];
+        this.lastName = newArr[1];
+
+
+    }
+}
+
+PersonForEx2.setFullName('Ahmet Rajabli');
+console.log(PersonForEx2.getFullName());
