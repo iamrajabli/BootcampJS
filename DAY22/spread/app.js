7439262719fbfb8f18492f25e61cb05a6d55385f
iamrajabli <iamrajabli@outlook.com>
2022-04-18 17:30:24 +0400

    Example 1

---
+++ DAY22/spread/app.js
@@ -0,0 +1,13 @@
+'use strict';
+let h1 = document.querySelector('h1');
+let divs = document.querySelectorAll('div');
+let myArr = [];
+
+// Example 1 
+// Spread with ES5 
+
+myArr.push(h1);
+divs.forEach(function(item) {
+    myArr.push(item);
+})
+console.log(myArr);
